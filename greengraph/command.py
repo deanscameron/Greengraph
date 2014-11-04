ea857101accb7511edf4c11c1d78ed19822d37c7
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2014-11-04 12:30:47 +0000

    Edited command file

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,5 +1,20 @@
 """Command file to execute greengraph"""
 
-from greengraph import greengraph 
+from argparse import ArgumentParser
+from greengraph import greengraph
 
-greengraph("London", "Birmingham", steps = 10)
+def process():
+   parser = ArgumentParser(description = "Run Greengraph between given cities")
+
+   parser.add_argument('start_city')
+   parser.add_argument('end_city')
+   parser.add_argument('steps')
+
+   arguments= parser.parse_args()
+   
+   greengraph(arguments.start_city, arguments.end_city, arguments.steps)
+
+
+if __name__ == "__main__":
+    process()
+
